3a680bd525beb6370ae5376c7b1e70b52dad5eec
Valerii Vasin <valerii.vasin@gmail.com>
2022-06-02 01:29:53 +0200

    chore: eslint prettier (#2)
    * chore: install and config eslint
    
    * refactor: prettify
    
    * chore: lint staged

--- src/app.tsx
+++ src/app.tsx
@@ -10,11 +10,7 @@ import Table from '@awsui/components-react/table';
 import { useState } from 'react';
 
 export function App() {
-  return <AppLayout
-    content={<Content />}
-    navigationHide
-    toolsHide
-    ></AppLayout>
+  return <AppLayout content={<Content />} navigationHide toolsHide></AppLayout>;
 }
 
 function Content() {
@@ -26,47 +22,45 @@ function Content() {
     { title: 'Адена', hourly: '500k', daily: '12kk' },
   ];
 
-  return <Container
-    header={<Header variant='h3'
-      actions={
-        <SpaceBetween size="s" direction='horizontal'>
-          <Button variant='normal'>Очистить</Button>
-        </SpaceBetween>
-      }>Калькулятор Фарма</Header>}
+  return (
+    <Container
+      header={
+        <Header
+          variant="h3"
+          actions={
+            <SpaceBetween size="s" direction="horizontal">
+              <Button variant="normal">Очистить</Button>
+            </SpaceBetween>
+          }
+        >
+          Калькулятор Фарма
+        </Header>
+      }
     >
-    <ColumnLayout columns={2} variant="text-grid">
-    <SpaceBetween direction='vertical' size='s'>
-    <FormField
-        label="Затраченное время"
-        constraintText="Например, 1ч 20м"
-        >
-        <Input value={time} onChange={(event) => setTime(event.detail.value)} />
-      </FormField>
+      <ColumnLayout columns={2} variant="text-grid">
+        <SpaceBetween direction="vertical" size="s">
+          <FormField label="Затраченное время" constraintText="Например, 1ч 20м">
+            <Input value={time} onChange={(event) => setTime(event.detail.value)} />
+          </FormField>
 
-      <FormField
-        label="Опыт"
-        constraintText="Например, 250kk"
-        >
-        <Input value={exp} onChange={(event) => setExp(event.detail.value)} />
-        </FormField>
+          <FormField label="Опыт" constraintText="Например, 250kk">
+            <Input value={exp} onChange={(event) => setExp(event.detail.value)} />
+          </FormField>
 
-        <FormField
-        label="Адена"
-        constraintText="Например, 15kk"
-        >
-        <Input value={adena} onChange={(event) => setAdena(event.detail.value)} />
-        </FormField>
+          <FormField label="Адена" constraintText="Например, 15kk">
+            <Input value={adena} onChange={(event) => setAdena(event.detail.value)} />
+          </FormField>
+        </SpaceBetween>
 
-      </SpaceBetween>
-
-      <Table
-        items={items}
-        columnDefinitions={[
-          { header: '', cell: item => item.title },
-          { header: 'За 1 час', cell: item => item.hourly },
-          { header: 'За 24 часа', cell: item => item.daily }
-        ]}
+        <Table
+          items={items}
+          columnDefinitions={[
+            { header: '', cell: (item) => item.title },
+            { header: 'За 1 час', cell: (item) => item.hourly },
+            { header: 'За 24 часа', cell: (item) => item.daily },
+          ]}
         ></Table>
-    </ColumnLayout>
-  </Container>
+      </ColumnLayout>
+    </Container>
+  );
 }
