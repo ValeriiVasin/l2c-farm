3b846d6db3d83b6f49bd091960105af7456452d2
Valerii Vasin <valerii.vasin@gmail.com>
2022-06-02 00:11:55 +0200

    feat: basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,66 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import AppLayout from '@awsui/components-react/app-layout';
+import ColumnLayout from '@awsui/components-react/column-layout';
+import Container from '@awsui/components-react/container';
+import FormField from '@awsui/components-react/form-field';
+import Input from '@awsui/components-react/input';
+import SpaceBetween from '@awsui/components-react/space-between';
+import Table from '@awsui/components-react/table';
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  return <AppLayout
+    content={<Content />}
+    navigationHide
+    toolsHide
+    ></AppLayout>
+}
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+function Content() {
+  const [exp, setExp] = useState('250kk');
+  const [time, setTime] = useState('1ч 20мин');
+  const [adena, setAdena] = useState('15кк');
+  const items = [
+    { title: 'Опыт', hourly: '5kkk', daily: '120kkk' },
+    { title: 'Адена', hourly: '500k', daily: '12kk' },
+  ];
+
+  return <Container header={'Калькулятор Фарма'}>
+    <ColumnLayout columns={2} variant="text-grid">
+    <SpaceBetween direction='vertical' size='s'>
+    <FormField
+        label="Затраченное время"
+        constraintText="Например, 1ч 20м"
+        >
+        <Input value={time} onChange={(event) => setTime(event.detail.value)} />
+      </FormField>
+
+      <FormField
+        label="Опыт"
+        constraintText="Например, 250kk"
+        >
+        <Input value={exp} onChange={(event) => setExp(event.detail.value)} />
+        </FormField>
+
+        <FormField
+        label="Адена"
+        constraintText="Например, 15kk"
+        >
+        <Input value={adena} onChange={(event) => setAdena(event.detail.value)} />
+        </FormField>
+
+      </SpaceBetween>
+
+      <Table
+        items={items}
+        columnDefinitions={[
+          { header: '', cell: item => item.title },
+          { header: 'За 1 час', cell: item => item.hourly },
+          { header: 'За 24 часа', cell: item => item.daily }
+        ]}
+        ></Table>
+    </ColumnLayout>
+  </Container>
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import "@awsui/global-styles/index.css"
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
