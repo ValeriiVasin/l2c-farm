c3d666c44051ff07a29c2202c5d4726a6a2f8b82
Valerii Vasin <valerii.vasin@gmail.com>
2022-06-02 01:23:27 +0200

    refactor: prettify

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import "@awsui/global-styles/index.css"
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './app'
+import '@awsui/global-styles/index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './app';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+);
