62f8b16444973e6e26f3c404b29026fe0b9642d2
Valerii Vasin <valerii.vasin@gmail.com>
2022-06-03 11:14:19 +0200

    test: cover basic functionality

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -1,3 +1,25 @@
+import { testId } from '../helpers/test-id';
+
+const selectors = {
+  results: testId('results'),
+  clearButton: testId('clear-button'),
+  inputs: {
+    adena: `${testId('adena')} input[type=text]`,
+    exp: `${testId('exp')} input[type=text]`,
+    time: `${testId('time')} input[type=text]`,
+  },
+  sections: {
+    hourly: {
+      adena: testId('section-adena-hourly'),
+      exp: testId('section-exp-hourly'),
+    },
+    daily: {
+      adena: testId('section-adena-daily'),
+      exp: testId('section-exp-daily'),
+    },
+  },
+};
+
 describe('basic tests', () => {
   beforeEach(() => {
     cy.visit('/');
@@ -5,5 +27,67 @@ describe('basic tests', () => {
 
   it('having proper tytle', () => {
     cy.title().should('equal', 'Фарм - Lineage 2 Classic');
+  });
+
+  it('do not display results on init', () => {
+    cy.get(selectors.results).should('not.exist');
+  });
+
+  it('calculates exp and adena', () => {
+    cy.get(selectors.inputs.exp).type('10kkk');
+    cy.get(selectors.inputs.adena).type('1kk');
+    cy.get(selectors.inputs.time).type('1h');
+
+    cy.get(selectors.sections.hourly.exp).should('have.text', '10kkk');
+    cy.get(selectors.sections.hourly.adena).should('have.text', '1kk');
+    cy.get(selectors.sections.daily.exp).should('have.text', '240kkk');
+    cy.get(selectors.sections.daily.adena).should('have.text', '24kk');
+  });
+
+  it('entering exp and time only', () => {
+    cy.get(selectors.inputs.exp).type('10kkk');
+    cy.get(selectors.inputs.time).type('1h');
+
+    cy.get(selectors.sections.hourly.exp).should('have.text', '10kkk');
+    cy.get(selectors.sections.hourly.adena).should('have.text', '-');
+    cy.get(selectors.sections.daily.exp).should('have.text', '240kkk');
+    cy.get(selectors.sections.daily.adena).should('have.text', '-');
+  });
+
+  it('entering adena and time only', () => {
+    cy.get(selectors.inputs.adena).type('1kk');
+    cy.get(selectors.inputs.time).type('1h');
+
+    cy.get(selectors.sections.hourly.exp).should('have.text', '-');
+    cy.get(selectors.sections.hourly.adena).should('have.text', '1kk');
+    cy.get(selectors.sections.daily.exp).should('have.text', '-');
+    cy.get(selectors.sections.daily.adena).should('have.text', '24kk');
+  });
+
+  it('results not visible when time not entered', () => {
+    cy.get(selectors.inputs.adena).type('1kk');
+    cy.get(selectors.inputs.exp).type('10kkk');
+
+    cy.get(selectors.results).should('not.exist');
+  });
+
+  describe('clear button', () => {
+    beforeEach(() => {
+      cy.get(selectors.inputs.exp).type('10kkk');
+      cy.get(selectors.inputs.adena).type('1kk');
+      cy.get(selectors.inputs.time).type('1h');
+    });
+
+    it('clear button clears all inputs', () => {
+      cy.get(selectors.clearButton).click();
+      cy.get(selectors.inputs.exp).should('have.value', '');
+      cy.get(selectors.inputs.adena).should('have.value', '');
+      cy.get(selectors.inputs.time).should('have.value', '');
+    });
+
+    it('clear button moves focus into exp input', () => {
+      cy.get(selectors.clearButton).click();
+      cy.get(selectors.inputs.exp).should('have.focus');
+    });
   });
 });
