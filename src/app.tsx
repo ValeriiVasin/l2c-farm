12f4166149cb8b528820431595b2a4d8df76ebe1
Valerii Vasin <valerii.vasin@gmail.com>
2022-06-15 02:22:24 +0200

    feat: dark mode

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,15 @@
 import AppLayout from '@awsui/components-react/app-layout';
+import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Exp } from './components/exp/exp';
 import { Farm } from './components/farm/farm';
 
 export function App() {
+  useEffect(() => {
+    const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    document.body.classList.toggle('awsui-dark-mode', isDarkMode);
+  }, []);
+
   return <AppLayout content={<Content />} navigationHide toolsHide></AppLayout>;
 }
 
